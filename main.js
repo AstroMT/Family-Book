85cdb3c159a8b0ca8f7e75a5d58193ca85ce3b2e
AstroMT <81778387+AstroMT@users.noreply.github.com>
2021-07-31 21:47:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var images = [
     "IMG_2926 (1).jpg", //Me!
-    "IMG_4036.PNG", //My sister
+    "IMG_2162.PNG", //My sister
     "IMG_3232.jpg", //My mom
-    "IMG_1263.jpg", //My dad
+    "image0.png", //My dad
     "IMG_8156.jpg", //My grandmother
     "IMG_8393.jpg", //My grandfather
-    "img_thumbnail.jpg", //Thumbnail
 ];
 
 var header = [
@@ -15,18 +14,16 @@ var header = [
     "This is my dad, Laxmi Narasimha", 
     "This is my grandmother, Padma", 
     "Lastly, this is my grandfather, Balachandra",
-    "My Family Book",
-]
+];
 
 var description = [
     "This is me! I'm a rising 6th grader and I love math, science, and computers.", 
-    "This is my older sister. She's 15 years old and will be going to 10th grade.",
+    "This is my older sister. She will be going to 10th grade.",
     "This is my mom, who is a software developer.",
     "This is my dad, who is a software engineer.",
     "This is my maternal grandmother.",
     "This is my maternal grandfather.",
-    "Click next to flip though it!",
-]
+];
   
 var i = 0;
   
